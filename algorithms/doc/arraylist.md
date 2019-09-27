50d5953a4d7d38cc59440288b764178de2b5a987
施杰灵 <shijieling123@163.com>
2019-09-27 17:36:32 +0800

    doc 说明

--- algorithms/doc/arraylist.md
+++ algorithms/doc/arraylist.md
@@ -1,1 +1,22 @@
 # ArrayList
+需要使用到的工具类
+```java
+System.arraycopy(Object src,  int  srcPos,Object dest, int destPos,int length);
+// src: 源数组
+// srcPos: 源数组的开始位置
+// dest: 目录数组
+// destPos: 目标数据的开始位置
+// length: 拷贝长度
+// 示例:
+public class Test{
+    public static void main(String[] args) {
+        Object[] o = {1};
+        Object[] o1 = new Object[2];
+        System.arraycopy(o, 0, o1, 0, o.length);
+        o1[1] = 2;
+        for (Object o2 : o1) {
+            System.out.println(o2);
+        }
+    }
+}
+```
--- algorithms/src/array/SArrayList.java
+++ algorithms/src/array/SArrayList.java
@@ -29,6 +29,53 @@ public class SArrayList<E> {
      * @param e
      */
     void add(E e) {
+        // 先检查容量是否够，如果够的话，直接添加。否则需要括容
+        size++;
+        if (!checkSize(size)) {
+            ensure();
+        }
+        object[size] = e;
+
+    }
+
+    void remove(E e) {
+        for (int i = 0; i < object.length; i++) {
+            if (object[i].equals(e)) {
+                // 将对应位置的元素置空
+                object[i] = null;
+                // 位置移动
+                System.arraycopy(object, i+1, object, i, size - object.length + 1);
+            }
+        }
+        size--;
+
+
+    }
+
+    private boolean checkSize(int size) {
+        return object.length < size;
+    }
+
+    private void ensure() {
+        // 括容， ArrayList 是  orignal + (orignal >> 1) 相当于是 1.5 倍。不过传入的是 size+1 , 所以应该是 1.5 倍 +1
+        Object[] objects = new Object[size << 1];
+        System.arraycopy(object,0, objects, 0, object.length);
+    }
+
+    public static void main(String[] args) {
+        // 位置移动
+        Object[] o1 = new Object[3];
+        o1[1] = 1;
+        o1[2] = 2;
+        for (int i = 0; i < o1.length; i++) {
+            if (o1[i] == null) {
+                System.arraycopy(o1, i + 1, o1, i, o1.length - i - 1);
+            }
+        }
+        o1[o1.length - 1] = null;
+        for (Object o : o1) {
+            System.out.println(o);
+        }
 
     }
 
