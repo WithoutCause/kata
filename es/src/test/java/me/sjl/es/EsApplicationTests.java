f5dbc9183a8e92fa24d953d1eb10578a02271e8d
withoutcause <shijieling123@163.com>
2021-01-19 20:52:31 +0800

    es测试提交

---
+++ es/src/test/java/me/sjl/es/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sjl.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
