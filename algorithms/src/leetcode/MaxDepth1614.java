8b984edfc306c867a254c039484f56734140ef54
施杰灵 <shijieling123@163.com>
2025-04-18 12:11:46 +0800

    1614.maxDepth

---
+++ algorithms/src/leetcode/MaxDepth1614.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class MaxDepth1614 {
+
+    public static int maxDepth(String s) {
+        char[] chars = s.toCharArray();
+        int size = 0;
+        int maxSize = 0;
+        for (char c : chars) {
+            if (c == '(') {
+                size++;
+                maxSize = Math.max(size, maxSize);
+            } else if (c == ')') {
+                size--;
+            }
+        }
+        return maxSize;
+    }
+
+    public static void main(String[] args) {
+        String s = "()(())((()()))";
+        int i = maxDepth(s);
+        System.out.println(i);
+    }
+}
