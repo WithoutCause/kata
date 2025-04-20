16ac1cfda09faaa9b36bb780ba4a0d339292dd58
施杰灵 <shijieling123@163.com>
2025-04-20 23:32:24 +0800

    32.LongestValidParentheses

---
+++ algorithms/src/leetcode/LongestValidParentheses32.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class LongestValidParentheses32 {
+
+    public static int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+
+    public static void main(String[] args) {
+        String s = "()(())";
+        int res = longestValidParentheses(s);
+        System.out.println(res);
+    }
+
+}
