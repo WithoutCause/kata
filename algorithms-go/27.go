5ceb191cbeeb4cae5e9ae9cf5aa5d8e3dd9b382e
施杰灵 <shijieling123@163.com>
2025-04-21 13:14:46 +0800

    27.removeElement

---
+++ algorithms-go/27.go
@@ -0,0 +1,16 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
+}
+
+func main() {
+
+}
