dc0c04ef2ed5f1768cfb986d1f8783522f5084b5
施杰灵 <shijieling123@163.com>
2025-05-01 22:01:47 +0800

    219.containsNearbyDuplicate2

---
+++ algorithms/src/main/java/leetcode/ContainsNearbyDuplicate219.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
+ * 如果存在，返回 true ；否则，返回 false 。
+ */
+public class ContainsNearbyDuplicate219 {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 1, 1};
+        boolean res = containsNearbyDuplicate(nums, 1);
+        System.out.println(res);
+    }
+
+}
