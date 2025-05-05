26a792cf588825b3eaa4ae65b2bef29c1ce3a799
施杰灵 <shijieling123@163.com>
2025-05-05 23:24:45 +0800

    242.isAnagram

---
+++ algorithms/src/main/java/leetcode/IsAnagram242.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsAnagram242 {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch : t.toCharArray()) {
+            Integer count = map.get(ch);
+            if (count == null) {
+                return false;
+            } else if (count > 1) {
+                map.put(ch, count - 1);
+            } else {
+                map.remove(ch);
+            }
+        }
+        return map.isEmpty();
+    }
+
+}
