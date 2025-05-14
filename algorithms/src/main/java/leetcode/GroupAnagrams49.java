57ce8f0d4bd063dc38d24af47c35d15b03977a43
施杰灵 <shijieling123@163.com>
2025-05-14 23:23:54 +0800

    49.groupAnagrams

---
+++ algorithms/src/main/java/leetcode/GroupAnagrams49.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.*;
+
+public class GroupAnagrams49 {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = groupAnagrams(strs);
+        System.out.println(lists);
+    }
+
+}
