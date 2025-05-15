81bc369d9ec07d2cd05f64f032874f9149a75913
施杰灵 <shijieling123@163.com>
2025-05-15 23:08:52 +0800

    496.nextGreaterElement

---
+++ algorithms/src/main/java/leetcode/NextGreaterElement496.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class NextGreaterElement496 {
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[] res = new int[m];
+        for (int i = 0; i < m; ++i) {
+            int j = 0;
+            while (j < n && nums2[j] != nums1[i]) {
+                ++j;
+            }
+            int k = j + 1;
+            while (k < n && nums2[k] < nums2[j]) {
+                ++k;
+            }
+            res[i] = k < n ? nums2[k] : -1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+        int[] ints = nextGreaterElement(nums1, nums2);
+        System.out.println(ints);
+    }
+
+}
