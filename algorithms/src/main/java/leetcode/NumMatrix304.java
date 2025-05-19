9fe5f61b98a8cfdef6da629b8d5427d6af285e6a
施杰灵 <shijieling123@163.com>
2025-05-19 22:28:10 +0800

    304.NumMatrix

---
+++ algorithms/src/main/java/leetcode/NumMatrix304.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+public class NumMatrix304 {
+
+    int[][] sums;
+
+    public NumMatrix304(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i][j + 1] = sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            sum += sums[i][col2 + 1] - sums[i][col1];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}
+        };
+        NumMatrix304 numMatrix = new NumMatrix304(matrix);
+        int res = numMatrix.sumRegion(2, 1, 4, 3);
+        System.out.println(res);
+    }
+
+}
